6cdb5b605189172719b312462841bb2bcb20bd1b
Fidel Ernesto Perez Bedoya <92316606+fidelpperez2021@users.noreply.github.com>
2026-01-26 13:46:33 -0500

    Add files via upload

---
+++ models/Noticia.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const NoticiaSchema = new mongoose.Schema({
+    titulo: {
+        type: String,
+        required: true
+    },
+    resumen: {
+        type: String,
+        required: true
+    },
+    contenido: {
+        type: String,
+        required: true
+    },
+    imagen: {
+        type: String,
+        required: true
+    },
+    categoria: {
+        type: String,
+        required: true
+    },
+    fecha: {
+        type: Date,
+        default: Date.now
+    },
+    autor: {
+        type: String,
+        default: "Comunicación GAD"
+    }
+});
+
+module.exports = mongoose.model('Noticia', NoticiaSchema);
